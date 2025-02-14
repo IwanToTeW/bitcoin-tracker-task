c4577f9d3c024be1a6d55ed0ed1bc8b1333d48ca
ITAscend <ivan.totev@ascend.agency>
2025-02-14 11:51:15 +0000

    Adds scheduled command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Jobs\NotifySubscribers;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::job(new NotifySubscribers())->hourlyAt(1);
+
